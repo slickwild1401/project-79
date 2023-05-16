ef734537a7293451df39b4ad1a2330c0830e5070
slickwild1401 <116937194+slickwild1401@users.noreply.github.com>
2023-05-16 17:01:33 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ menu_list_array = ["Veg Margherita Pizza",
 		var imgtags='<img id="im1" src="pizzaImg.png">'
         var item=document.getElementById("add_item").value;
         // use the sort function as - menu_list_array.sort();
-        var sort=menu_list_array.sort("add_item").value;
+        var sort=menu_list_array.sort();
         htmldata="";
         for(var i=0;i<menu_list_array.length;i++){
             htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
